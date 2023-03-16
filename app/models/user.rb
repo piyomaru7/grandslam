b7dfbbc98b6c2fa84b09c80089077bd6d401d3c9
piyomaru7 <kbtslugger14@gmail.com>
2023-03-16 11:37:04 +0900

    userモデルの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :user_name, presence: true
+  validates :user_name, presence: true, uniqueness: true
   
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
