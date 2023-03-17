958c5e9924d6c4abfb72d694d7fc8d2f572d2630
piyomaru7 <kbtslugger14@gmail.com>
2023-03-17 16:52:47 +0900

    投稿削除機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new,:edit]
-  before_action :move_to_index, only: [:edit,:update]
+  before_action :authenticate_user!, only: [:new,:edit,:destroy]
+  before_action :set_post, only: [:show,:edit,:update,:destroy,:move_to_index]
+  before_action :move_to_index, only: [:edit,:update,:destroy]
+  
 
   def index
     @posts = Post.order(created_at: :DESC).includes(:user)
@@ -20,20 +22,22 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to post_path(@post)
     else
       render :edit
     end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
   private
@@ -42,8 +46,11 @@ class PostsController < ApplicationController
     params.require(:post).permit(:text,:image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
+  def set_post
     @post = Post.find(params[:id])
+  end
+
+  def move_to_index
     return if current_user.id == @post.user_id
 
     redirect_to action: :index
