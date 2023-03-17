ff31cfbc31476794fdb10493d033d00ea082ad55
piyomaru7 <kbtslugger14@gmail.com>
2023-03-17 16:42:37 +0900

    投稿編集機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new,:edit]
+  before_action :move_to_index, only: [:edit,:update]
 
   def index
     @posts = Post.order(created_at: :DESC).includes(:user)
@@ -22,9 +23,29 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:text,:image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @post = Post.find(params[:id])
+    return if current_user.id == @post.user_id
+
+    redirect_to action: :index
   end
 end
