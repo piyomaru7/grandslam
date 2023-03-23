38fd398d2dac6bfcc8d169102573acfe31f0c9d6
piyomaru7 <kbtslugger14@gmail.com>
2023-03-23 11:12:08 +0900

    ユーザー情報編集機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   validates :user_name, presence: true, uniqueness: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX
+  validates_format_of :password, with: PASSWORD_REGEX, on: :create
 
   # ユーザーをフォローする
   def follow(user_id)
